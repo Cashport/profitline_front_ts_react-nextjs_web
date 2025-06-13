33aa577886f19e1541e1c663df6cacb593fa4d75
FedericoC <federicoc03@gmail.com>
2025-06-13 11:27:09 -0500

    feat: add commentary input to ModalApplyAI and adjust styles

--- src/modules/clients/containers/apply-tab/Modals/ModalApplyAI/ModalApplyAI.tsx
+++ src/modules/clients/containers/apply-tab/Modals/ModalApplyAI/ModalApplyAI.tsx
@@ -35,6 +35,7 @@ const ModalApplyAI = ({ isOpen, onClose, mutate }: Props) => {
   const clientId = extractSingleParam(params.clientId) || "";
 
   const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+  const [commentary, setCommentary] = useState<string>();
   const [loading, setLoading] = useState(false);
 
   const closeModal = () => {
@@ -42,9 +43,15 @@ const ModalApplyAI = ({ isOpen, onClose, mutate }: Props) => {
     setUploadedFiles([]);
   };
 
+  const handleOnChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentary(e.target.value);
+  };
+
   const handleAnalyzeFiles = async () => {
     setLoading(true);
     try {
+      // TODO: Implement the logic to add the commentary to the request
+      console.info("commentary", commentary);
       await applyWithCashportAI(projectId, clientId, uploadedFiles);
       message.success("Archivos analizados con CashportAI");
       mutate();
@@ -162,6 +169,12 @@ const ModalApplyAI = ({ isOpen, onClose, mutate }: Props) => {
         <p className="draggerText">Arrastra y suelta tu archivo aquí o haz clic para subirlo</p>
         <p className="draggerText -small">Tamaño máximo 5MB</p>
       </Dragger>
+
+      <div className="modalApplyAI__commentary">
+        <p>Comentario</p>
+        <textarea onChange={handleOnChangeTextArea} placeholder="Ingresar un comentario" />
+      </div>
+
       <div className="modalApplyAI__footer">
         <Button className="cancelButton" onClick={() => onClose(true)}>
           Cancelar
