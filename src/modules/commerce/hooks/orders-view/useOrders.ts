b4dd7c2e80b16d920999da6586778af0f489789d
FedericoC <federicoc03@gmail.com>
2025-08-06 01:22:37 -0500

    feat: implement useOrders hook for fetching marketplace orders with filters

---
+++ src/modules/commerce/hooks/orders-view/useOrders.ts
@@ -0,0 +1,35 @@
+import useSWR from "swr";
+import { useAppStore } from "@/lib/store/store";
+import { fetcher } from "@/utils/api/api";
+import { IMarketplaceOrderFilters } from "@/components/atoms/Filters/FilterMarketplaceOrders/FilterMarketplaceOrders";
+import { IOrderData } from "@/types/commerce/ICommerce";
+import { GenericResponse } from "@/types/global/IGlobal";
+
+interface UseOrdersProps {
+  selectedFilters: IMarketplaceOrderFilters;
+}
+
+export const useOrders = ({ selectedFilters }: UseOrdersProps) => {
+  const { ID: projectId } = useAppStore((state) => state.selectedProject);
+
+  // Construir query params de manera simple
+  const sellersQuery =
+    selectedFilters.sellers && selectedFilters.sellers.length > 0
+      ? `?sellers=${selectedFilters.sellers.join(",")}`
+      : "";
+
+  // Path completo para SWR
+  const pathKey = `/marketplace/projects/${projectId}/orders${sellersQuery}`;
+
+  const { data, error, mutate } = useSWR<GenericResponse<IOrderData[]>>(pathKey, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+  });
+
+  return {
+    ordersByCategory: data?.data,
+    isLoading: !error && !data,
+    isError: error,
+    mutate
+  };
+};
