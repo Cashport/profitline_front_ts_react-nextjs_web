677c87a12e1aa318352dc0bc5a1f85ca94df0af6
FedericoC <federicoc03@gmail.com>
2025-08-12 17:18:09 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,7 +2,11 @@ import ChatInbox from "@/modules/chat/containers/chat-inbox";
 
 export default function Page() {
   return (
+<<<<<<< HEAD
     <main className="h-dvh w-full bg-white text-[#141414] rounded-lg">
+=======
+    <main className="h-dvh w-full bg-white text-[#141414]">
+>>>>>>> 0b72e874 (Refactor code structure for improved readability and maintainability)
       <ChatInbox />
     </main>
   );
