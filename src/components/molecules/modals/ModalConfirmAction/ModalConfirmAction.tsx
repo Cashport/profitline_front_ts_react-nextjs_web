670378ba5b1a7f5a602e1e4b84ac4e641f9aa013
Federico <federicoc03@gmail.com>
2025-11-21 15:15:34 -0500

    Enhance responsiveness of modals and containers for better mobile experience

--- src/components/molecules/modals/ModalConfirmAction/ModalConfirmAction.tsx
+++ src/components/molecules/modals/ModalConfirmAction/ModalConfirmAction.tsx
@@ -30,7 +30,7 @@ export const ModalConfirmAction = ({
   return (
     <Modal
       className="ModalConfirmAction"
-      width={"50%"}
+      // width={"50%"}
       open={isOpen}
       onCancel={onClose}
       okButtonProps={{ className: "acceptButton", loading: okLoading }}
