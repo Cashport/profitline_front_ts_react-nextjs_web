f006997e2b36bb47cfa626d90ba85ab8302c3344
Marcos <fangarita@profitline.com.co>
2025-12-16 16:13:40 -0300

    Add error and loader components to CashportMobile
    Introduced ErrorMobile and MobileLoader components for improved error handling and loading states in the CashportMobileView. Updated ESLint config for better TypeScript support. Refactored CashportMobileView to use new components and handle errors and loading more gracefully.

---
+++ src/modules/cashportMobile/components/Loader/MobileLoader.tsx
@@ -0,0 +1,10 @@
+import Loader from "@/components/atoms/loaders/loader";
+import "./mobileLoader.scss";
+
+export default function MobileLoader() {
+  return (
+    <div className="cashportMobileView">
+      <Loader></Loader>
+    </div>
+  );
+}
