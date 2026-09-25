fa98a69adbf64c6b50275b921fdcb29ebe7f9e9d
Federico C <federicoc03@gmail.com>
2026-09-25 09:17:16 -0500

    Remove commented-out code and unused imports in header component

--- src/components/atoms/NavigationBar/ModulesButtons/ModulesButtons.tsx
+++ src/components/atoms/NavigationBar/ModulesButtons/ModulesButtons.tsx
@@ -109,7 +109,7 @@ export const ModulesButtons = ({ path, project, isMobileMenu = false }: ModulesB
       )} */}
 
       {/* Notificaciones */}
-      {checkUserViewPermissions(project, "Notificaciones") && (
+      {/* {checkUserViewPermissions(project, "Notificaciones") && (
         <Link href="/notificaciones" passHref legacyBehavior>
           <Button
             type="primary"
@@ -121,7 +121,7 @@ export const ModulesButtons = ({ path, project, isMobileMenu = false }: ModulesB
             onClick={(e) => handleNavClick(e, "/notificaciones")}
           />
         </Link>
-      )}
+      )} */}
 
       {/* Marketplace / Comercio */}
       {checkUserViewPermissions(project, "Marketplace") && (
