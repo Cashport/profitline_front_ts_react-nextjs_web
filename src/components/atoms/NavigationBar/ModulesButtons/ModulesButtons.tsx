ed9770b1921ebee81104404baa7eeb27f6726cd9
Federico C <federicoc03@gmail.com>
2026-09-25 09:17:39 -0500

    Hide buttons in sidebar based on user permissions

--- src/components/atoms/NavigationBar/ModulesButtons/ModulesButtons.tsx
+++ src/components/atoms/NavigationBar/ModulesButtons/ModulesButtons.tsx
@@ -150,7 +150,7 @@ export const ModulesButtons = ({ path, project, isMobileMenu = false }: ModulesB
       )}
 
       {/* Configuración / Settings */}
-      {checkUserViewPermissions(project, "Configuracion") && (
+      {/* {checkUserViewPermissions(project, "Configuracion") && (
         <Link href="/settings" passHref legacyBehavior>
           <Button
             type="primary"
@@ -164,7 +164,7 @@ export const ModulesButtons = ({ path, project, isMobileMenu = false }: ModulesB
             onClick={(e) => handleNavClick(e, "/settings")}
           />
         </Link>
-      )}
+      )} */}
 
       {/* Proveedores */}
       {/* {checkUserViewPermissions(project, "Proveedores") && (
